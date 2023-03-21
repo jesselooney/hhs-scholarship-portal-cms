a2f0ff509120a54ae33d079244703495e3e012d6
Impartiall <42242696+Impartiall@users.noreply.github.com>
2023-03-20 22:38:22 -0400

    Scaffold dashboard UI

--- src/plugins/dashboard/admin/src/pages/HomePage/index.tsx
+++ src/plugins/dashboard/admin/src/pages/HomePage/index.tsx
@@ -4,18 +4,123 @@
  *
  */
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import pluginId from "../../pluginId";
-import { DatePicker } from "@strapi/design-system";
+import {
+  Badge,
+  Box,
+  Button,
+  Stack,
+  DatePicker,
+  GridLayout,
+  Typography,
+} from "@strapi/design-system";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
-      <DatePicker label="Date picker" />
-    </div>
+    <Stack padding={8} spacing={8}>
+      <PortalDateConfiguration />
+      <ConfigurationCard title="Import">
+        <Button>Upload Scholarships</Button>
+      </ConfigurationCard>
+      <ConfigurationCard title="Export">
+        <Button>Download Applications</Button>
+      </ConfigurationCard>
+      <ConfigurationCard title="Administration">
+        <Button variant="danger">Purge Data</Button>
+      </ConfigurationCard>
+    </Stack>
   );
 };
+
+type PortalStatus = "unconfigured" | "open" | "closed";
+
+const PortalDateConfiguration = () => {
+  const [startDate, setStartDate] = useState<Date>();
+  const [endDate, setEndDate] = useState<Date>();
+  const [error, setError] = useState("");
+  const [portalStatus, setPortalStatus] =
+    useState<PortalStatus>("unconfigured");
+
+  useEffect(() => {
+    if (
+      startDate !== undefined &&
+      endDate !== undefined &&
+      startDate > endDate
+    ) {
+      setError("End date cannot be before start date.");
+      setPortalStatus("unconfigured");
+    } else {
+      setError("");
+      if (startDate !== undefined && endDate !== undefined) {
+        const today = new Date();
+        setPortalStatus(
+          startDate <= today && today <= endDate ? "open" : "closed"
+        );
+      }
+    }
+  }, [startDate, endDate]);
+
+  return (
+    <ConfigurationCard
+      title="Application Submission Window"
+      description="Configure the time period during which students may access the portal."
+    >
+      <Badge marginBottom={2}>status: {portalStatus}</Badge>
+      <GridLayout>
+        <DatePicker
+          onChange={setStartDate}
+          selectedDate={startDate}
+          label="Opens"
+          name="datepicker-start"
+          clearLabel="Clear the date picker"
+          onClear={() => setStartDate(undefined)}
+          selectedDateLabel={(formattedDate: any) =>
+            `Date picker, current is ${formattedDate}`
+          }
+        />
+        <DatePicker
+          onChange={setEndDate}
+          selectedDate={endDate}
+          label="Closes"
+          name="datepicker-end"
+          clearLabel="Clear the date picker"
+          onClear={() => setEndDate(undefined)}
+          selectedDateLabel={(formattedDate: any) =>
+            `Date picker, current is ${formattedDate}`
+          }
+          error={error}
+        />
+      </GridLayout>
+    </ConfigurationCard>
+  );
+};
+
+interface ConfigurationCardProps {
+  title: string;
+  description?: string;
+  children?: React.ReactNode;
+}
+
+const ConfigurationCard = ({
+  title,
+  description,
+  children,
+}: ConfigurationCardProps) => (
+  <Box padding={4} hasRadius maxWidth="80ch" background="neutral0">
+    <Stack>
+      <Typography variant="beta">{title}</Typography>
+      <Typography variant="epsilon">{description}</Typography>
+    </Stack>
+    <Box marginTop={4}>{children}</Box>
+  </Box>
+);
+
+interface SimpleDatePickerProps {
+  date: any;
+  setDate: any;
+  label: string;
+  name: string;
+}
 
 export default HomePage;
